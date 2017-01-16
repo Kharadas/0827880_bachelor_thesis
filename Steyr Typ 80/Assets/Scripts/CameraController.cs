bccf0813d5424f8b92c6b728774eeca04c04ef52
SIGISMOND\Onur <onurgurcay@outlook.com>
2017-01-16 18:45:26 +0100

    Camera and object locations changed based on control unit, First scripts written to move the tractor and the camera with it. Various components added to tractor and floor objects to make them responsive to the environment.

---
+++ Steyr Typ 80/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject tractor;
+
+    private Vector3 offset;
+    
+	void Start () {
+        offset = transform.position - tractor.transform.position;
+	}
+	
+	void LateUpdate () {
+        transform.position = tractor.transform.position + offset;
+	}
+}
